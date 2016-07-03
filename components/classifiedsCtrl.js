388b63e8c51f0f61e3fa8d9f840f64f43d393e56
Fernando <ffmp777@yahoo.com>
2016-07-02 22:25:44 -0600

    sidenav

--- components/classifiedsCtrl.js
+++ components/classifiedsCtrl.js
@@ -2,17 +2,25 @@
   "use strict";
   angular
     .module('ngClassifieds')
-    .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory) {
+    .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory, $mdSidenav) {
 
-//$http returns promisies, which aid with Javascript's asynchronous nature.  
-//This means that code can continue to execute and not be blocked while 
+//$http returns promisies, which aid with Javascript's asynchronous nature.
+//This means that code can continue to execute and not be blocked while
 //the "promise" continues processing a privious instruction(code).
 	classifiedsFactory.getClassifieds().then(function(classifieds) {
 		  //the method getClassifieds comes from our factory.
 		$scope.classifieds = classifieds.data;
+    $scope.openSidebar = function() {
+      //sidebar open
+      $mdSidenav('left').open();
+    }
+    $scope.closeSidebar = function() {
+      //sidebar close
+      $mdSidenav('left').close();
+    }
 	});
 
-      // $scope.classifieds = 
+      // $scope.classifieds =
 
       // $scope.message = "Hello, Fernando!";
 
